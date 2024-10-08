1e3f7560e11d574367ba53d6a44ec74c1286361d
p4n1k0 <gabre.tarick@gmail.com>
2024-10-08 16:30:19 -0300

    endpoint GET /top-secret

--- src/app.js
+++ src/app.js
@@ -21,6 +21,15 @@ app.use(
 app.get('/ping', controllers.ping);
 app.post('/login', controllers.login);
 app.get('/users/me', middlewares.auth, controllers.me);
+app.get(
+  '/top-secret',
+  /* Middleware que valida o JWT e cria `req.user` */
+  middlewares.auth,
+  /* Middleware que verifica se a pessoa autenticada é admin */
+  middlewares.admin,
+  /* Controller do endpoint */
+  controllers.topSecret,
+);
 
 app.use(middlewares.error);
 
